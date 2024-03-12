b413cd99f6f74d68eb598867246369df0f73424b
Sanjay Rautela <140192191+sanjayrautela@users.noreply.github.com>
2024-03-12 04:28:38 +0000

    question done

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 
 import React from "react";
 import './../styles/App.css';
+import Counter from "./Counter";
+
 
 const App = () => {
   return (
     <div>
+      <Counter/>
         {/* Do not remove the main div */}
     </div>
   )
---
+++ src/components/counter.js
@@ -0,0 +1,30 @@
+
+
+
+import React, { Component } from 'react'
+
+
+class counter extends Component {
+    constructor(props){
+        super(props);
+        this.state = {count:0};
+    }
+  render() {
+    const incrementCount = () =>{
+        const curr = this.state.count;
+        this.setState({count:1 + curr})
+    }
+
+
+
+    return (
+      <div>
+        <p>Button clicked {this.state.count} Times</p>
+        <button onClick={incrementCount}>Click me</button>
+        
+      </div>
+    )
+  }
+}
+
+export default counter;
